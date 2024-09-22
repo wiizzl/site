a1d658a82ed32e045c15d07fcadfc8643026a3e2
wiizzl <wiizz76.contact@gmail.com>
2024-09-22 16:18:44 +0200

    fix: rollack authjs pages

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,8 @@ import { prisma } from "@/lib/prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
     pages: {
         error: "/error",
+        signIn: "/",
+        signOut: "/",
     },
     adapter: PrismaAdapter(prisma),
     events: {
