f1e29cb55588451e3aa246666045c63e4a09debc
wiizzl <wiizz76.contact@gmail.com>
2025-01-12 20:07:36 +0100

    feat: optimize imports in header component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,8 +1,8 @@
+import { Mail } from "lucide-react";
 import Link from "next/link";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { Mail } from "lucide-react";
 
 import { getInitials } from "@/lib/utils";
 
